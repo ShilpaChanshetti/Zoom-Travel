0a67a8a8bf154ba1a48c4aa79f38534df3610dc3
chioujiin <a109133409@gmail.com>
2020-04-02 21:49:01 -0500

    connected to about us

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+# from flask import Flask, render_template
+# import datetime
+# import re
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     #print('s')
+#     return render_template('index.html')
+
 from flask import Flask, render_template
-import datetime
-import re
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    #print('s')
     return render_template('index.html')
+
+@app.route('/about')
+def aboutUs():
+    return render_template('about-us.html')
+
+if __name__ == '__main__':
+    app.run()
